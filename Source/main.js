ed405bac6388db051b2098d32f7c0e6016527651
Alexander Davidson <alexander.harlan.davidson@gmail.com>
2017-04-04 17:43:26 -0700

    Huh

--- Source/main.js
+++ Source/main.js
@@ -6,7 +6,30 @@ $('.ui.dropdown')
   .dropdown()
 ;
 
+$('.ui.modal')
+  .modal()
+;
+
+$('.content.example .ui.embed').embed();
+
+$(document).ready(function(){
+     $('#modal-350r-button').click(function(){
+        $('#modal-350r').modal('show');
+     });
+});
+
 var $shipPulse = document.getElementById('ship-par-85x')
+
+$shipPulse.addEventListener("mouseover", function( event ) {
+    // highlight the mouseover target
+    event.target.classList.add('animated', 'pulse')
+
+    setTimeout(function() {
+      event.target.classList.remove('animated', 'pulse')
+    }, 1000);
+  }, false);
+
+var $shipPulse = document.getElementById('ship-par-m50')
 
 $shipPulse.addEventListener("mouseover", function( event ) {
     // highlight the mouseover target
